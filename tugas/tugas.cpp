33e21670cdebb3974bdbb6d7e9853239d36d85a4
ZakyMalika <zakymalika0987@gmail.com>
2024-05-11 12:11:31 +0700

    membuat class push

--- tugas/tugas.cpp
+++ tugas/tugas.cpp
@@ -18,4 +18,12 @@ private:
 	Node* top;	// pointer to the top node of the stack
 
 public:
+	Stack() {
+		top = NULL; // initialize the stack with a null top pointer
+	}
 };
+
+// push operation: insert an element onto the top of stack
+int push(int value) {
+
+}
