b9ee1e5fb3ef66e419509f89d1e279c9ffcd5af1
ZakyMalika <zakymalika0987@gmail.com>
2024-05-11 12:15:10 +0700

    membuat fungsi pop

--- tugas/tugas.cpp
+++ tugas/tugas.cpp
@@ -26,5 +26,15 @@ public:
 // push operation: insert an element onto the top of stack
 int push(int value) {
 	Node* newNode = new Node();
-	newNode->
+	newNode->data = value;
+	newNode->next = top;
+	top = newNode;
+	cout << "Push value: " << value << endl;
+	return value;
+
+}
+
+// pop operation: 
+void pop() {
+	if 
 }
